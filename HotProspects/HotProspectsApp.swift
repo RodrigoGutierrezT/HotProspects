f6fa0d4980294fd9d914888d1e3840baa3d1a511
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-08-26 22:00:14 -0400

    Initial commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Rodrigo on 26-08-24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
